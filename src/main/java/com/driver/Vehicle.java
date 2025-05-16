97e64722a09598381c6944aca39249cbf9569097
Vishalwani123 <93905384+Vishalwani123@users.noreply.github.com>
2025-05-16 11:52:33 +0530

    Assignment completed

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -12,6 +12,18 @@ public class Vehicle {
         this.currentDirection = 0;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
         this.currentDirection += direction;
@@ -30,12 +42,6 @@ public class Vehicle {
         System.out.println("stop method called - The vehicle is stopped");
     }
 
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
 
-    public int getCurrentDirection() {
-        return currentDirection;
-    }
 
 }
